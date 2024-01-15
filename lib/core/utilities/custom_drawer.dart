39d6e3612c07dac0e9929c35b95412f1c02cac5a
Hetzie <hetvipatel1952@gmail.com>
2024-01-15 16:31:28 +0530

    added gradient_text.dart custom_drawer.dart

---
+++ lib/core/utilities/custom_drawer.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:streaming_data_dashboard/core/utilities/gradient_text.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      width: 250,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+              decoration: BoxDecoration(color: Colors.grey),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: GradientText(
+                    "adani",
+                    gradient: LinearGradient(colors: [
+                      Color(0xff0B74B0),
+                      Color(0xff75479C),
+                      Color(0xffBD3861),
+                    ]),
+                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                  ))),
+          ListTile(
+            title: const Text(
+              ' Mundra ',
+              style: TextStyle(color: Colors.black),
+            ),
+            onTap: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
