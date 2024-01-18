e40e9fcd18e100bdd01bb52b0e68eba7d794a870
dhriti987 <dhritimanbhardwaj@gmail.com>
2024-01-18 13:34:54 +0530

    minor changes

---
+++ lib/features/settings/UI/client_credentials_settings_widget.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ClientCredentialSettings extends StatelessWidget {
+  const ClientCredentialSettings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: size.width / 4,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  labelText: "Client ID",
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  labelText: "Client Secret",
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: const StadiumBorder(),
+                  padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                ),
+                child: Text(
+                  "Update",
+                  style: TextStyle(fontSize: size.height / 40),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/settings/UI/settings_page.dart
+++ lib/features/settings/UI/settings_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:streaming_data_dashboard/features/settings/UI/client_credentials_settings_widget.dart';
+import 'package:streaming_data_dashboard/features/settings/UI/plants_settings_widget.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
 
   @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  final List<Widget> settingWidgets = const [
+    ClientCredentialSettings(),
+    PlantsAndUnitSettings(),
+  ];
+
+  int currentSettings = 1;
+
+  @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Settings"),
@@ -14,14 +29,62 @@ class SettingsPage extends StatelessWidget {
         children: [
           Expanded(
             flex: 2,
-            child: Container(
-              color: Colors.amber,
+            child: ListView(
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: (currentSettings == 0)
+                        ? Colors.grey.shade400
+                        : Colors.transparent,
+                    elevation: 0,
+                    foregroundColor: Colors.black,
+                    fixedSize: Size.fromHeight(
+                      size.height / 15,
+                    ),
+                    shadowColor: Colors.black12,
+                    alignment: Alignment.centerLeft,
+                  ),
+                  child: Text(
+                    "Client Credentials",
+                    style: TextStyle(fontSize: size.height / 40),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: (currentSettings == 1)
+                        ? Colors.grey.shade400
+                        : Colors.transparent,
+                    elevation: 0,
+                    foregroundColor: Colors.black,
+                    fixedSize: Size.fromHeight(
+                      size.height / 15,
+                    ),
+                    shadowColor: Colors.black12,
+                    alignment: Alignment.centerLeft,
+                  ),
+                  child: Text(
+                    "Plants",
+                    style: TextStyle(fontSize: size.height / 40),
+                  ),
+                ),
+              ],
             ),
+          ),
+          const VerticalDivider(
+            width: 20,
+            thickness: 3,
+            indent: 20,
+            endIndent: 20,
+            color: Colors.black12,
           ),
           Expanded(
             flex: 6,
-            child: Container(
-              color: Colors.red,
+            child: SizedBox(
+              height: double.maxFinite,
+              width: double.maxFinite,
+              child: settingWidgets[currentSettings],
             ),
           )
         ],
