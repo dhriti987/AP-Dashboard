0b2e18bceea5552134197f3373ba72b2a47ea8c3
dhriti987 <dhritimanbhardwaj@gmail.com>
2024-01-24 10:58:52 +0530

    setting minimum size of window

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,14 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:streaming_data_dashboard/core/router/router.dart';
 import 'package:streaming_data_dashboard/core/theme/common_theme.dart';
 import 'package:streaming_data_dashboard/service_locator.dart';
+import 'package:window_manager/window_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
   setup();
   await dotenv.load(fileName: ".env");
+  await windowManager.setMinimumSize(const Size(1000, 600));
   sl.allReady().then((value) {
     runApp(const MyApp());
   });
